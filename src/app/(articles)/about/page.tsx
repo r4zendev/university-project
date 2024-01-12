9b3eda2525377aa218dabe759cf6672732402fc4
Sasha Mysak <reactionnnnn@gmail.com>
2024-01-12 13:04:45 +0100

    final code

--- src/app/(articles)/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-  return (
-    <p className="flex min-h-screen flex-col items-center justify-center">
-      I want to be statically generated
-    </p>
-  );
-}
-
-export const dynamic = "force-static";
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
+import type { NextRequest, NextResponse } from "next/server";
 
 import { env } from "~/env.mjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  // publicRoutes: ["/", "/contact"],
-  publicRoutes: ["((?!^/admin/).*)"],
-  // debug: env.NODE_ENV === "development",
-});
+const middleware = async (req: NextRequest): Promise<NextResponse<unknown>> => {
+  const url = req.nextUrl;
+
+  return authMiddleware({
+    // publicRoutes: ["/", "/contact"],
+    publicRoutes: ["((?!^/admin/).*)"],
+    // debug: env.NODE_ENV === "development",
+  });
+};
+
+export default middleware;
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
